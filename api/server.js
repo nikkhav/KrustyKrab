782881c4dc4679481a3174835e458963e7a6b046
nikkhav <103598295+nikkhav@users.noreply.github.com>
2022-09-25 13:50:19 +0200

    Deploy commit 2

--- api/server.js
+++ api/server.js
@@ -3,7 +3,9 @@ const dotenv = require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 4000;
 const mongoose = require("mongoose");
 
-const DB = process.env.DATABASE.replace("<password>", process.env.DB_PASSWORD);
+//const DB = process.env.DATABASE.replace("<password>", process.env.DB_PASSWORD);
+const DB =
+  "mongodb+srv://nikkhav:eiNfj2YTMuJEm0G3@menu.lapfejg.mongodb.net/KrustyKrabs?retryWrites=true";
 
 mongoose
   .connect(DB, {
